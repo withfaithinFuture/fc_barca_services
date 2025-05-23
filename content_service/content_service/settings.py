fcb30d3bc5242294533cdb22215d8a5ae87efb7c
withfaithinFuture <110636640+withfaithinFuture@users.noreply.github.com>
2025-05-23 12:48:28 +0300

    Update settings.py

--- content_service/content_service/settings.py
+++ content_service/content_service/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'auth_service', 'content_service']
 
 
 
@@ -144,9 +144,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-
-
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'auth_service', 'content_service']
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
